e38fab9bd814bcd9ec06e0146b185557d4d7d66c
Ilya Korchenov <ikorchenov@bk.ru>
2017-12-08 14:15:26 +0300

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ zip -r build.zip build
 curl -H "Content-type: application/zip" \
     -H "Authorization: Bearer $NETLIFY_KEY" \
     --data-binary "@build.zip" \
-    -I https://api.netlify.com/api/v1/sites/cocky-leavitt-d2f1d4
+    https://api.netlify.com/api/v1/sites/cocky-leavitt-d2f1d4
